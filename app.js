936d462a61050ffe974065253703aaa5bb2788b5
debindv <debinptpm@gmail.com>
2020-03-16 21:00:18 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+var Web3 = require("web3");
+
+web3 = new Web3("http://localhost:7545");
+
+const app = express();
+
+coinbase = "0x0746b306b201889498da9c69963f42d5b01f864f";
+var contractAddress = "0xdbc7ff572a0c0697a53e97179a16b8dc9c81427f";
+
+var contractAbi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "candidatesCount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "candidates",
+    "outputs": [
+      {
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "name": "voteCount",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "candidate",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "voters",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_candidateId",
+        "type": "uint256"
+      }
+    ],
+    "name": "vote",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+
+Election = new web3.eth.Contract(contractAbi, contractAddress);
+
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
+
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+// For static front end
+app.use(express.static('front-end'));
+
+// Routes
+
+app.use('/register', require('./routes/register'));
+app.use('/login', require('./routes/login'));
+app.use('/dashboard', require('./routes/dashboard'));
+
+
+
+
+
+
+module.exports = app;
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, console.log(`Server started on port ${PORT}`));
